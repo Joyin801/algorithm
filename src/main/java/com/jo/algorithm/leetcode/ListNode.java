3e8b82efd7448e8d4958bc62547440295137546e
Joyin801 <yxq801@gmail.com>
2024-11-02 01:33:18 +0800

    init

---
+++ src/main/java/com/jo/algorithm/leetcode/ListNode.java
@@ -0,0 +1,9 @@
+package com.jo.algorithm.leetcode;
+
+/**
+ * Date:2024/11/2
+ * Author:Jo
+ * Description:
+ */
+public class ListNode {
+}
---
+++ src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode1.java
@@ -0,0 +1,9 @@
+package com.jo.algorithm.leetcode.hot100;
+
+/**
+ * Date:2024/11/2
+ * Author:Jo
+ * Description:1.
+ */
+public class LeetCode1 {
+}
