5cedca255f5a109d16d953f23f725faff2acb503
Joyin801 <yxq801@gmail.com>
2024-11-07 23:01:09 +0800

    LeetCode560

--- src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode560.java
+++ src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode560.java
@@ -1,5 +1,8 @@
 package com.jo.algorithm.leetcode.hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Date:2024/11/7
  * Author:Jo
@@ -8,7 +11,17 @@ package com.jo.algorithm.leetcode.hot100;
 public class LeetCode560 {
     class Solution {
         public int subarraySum(int[] nums, int k) {
-
+            int count = 0, sum = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+                if (map.containsKey(sum - k)) {
+                    count += map.get(sum - k);
+                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+            return count;
         }
     }
 }
--- src/test/java/com/jo/algorithm/AlgorithmApplicationTests.java
+++ src/test/java/com/jo/algorithm/AlgorithmApplicationTests.java
@@ -12,6 +12,20 @@ class AlgorithmApplicationTests {
     }
 
     class Solution {
+        public ListNode swapPairs2(ListNode head) {
+            ListNode dummy = new ListNode(0,head);
+            ListNode temp = dummy;
+            while(temp.next != null && temp.next.next != null){
+                ListNode node1 = temp.next;
+                ListNode node2 = temp.next.next;
+                temp.next = node2;
+                node1.next = node2.next;
+                node2.next = node1;
+                temp = node1;
+            }
+            return dummy.next;
+        }
+
         public ListNode removeNthFromEnd(ListNode head, int n) {
             ListNode dummy = new ListNode(0, head);
             ListNode fast = head;
