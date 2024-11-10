624e55cd196d05c9d5c947faf8e15fc06572cdda
Joyin801 <yxq801@gmail.com>
2024-11-10 23:08:44 +0800

    LeetCode35

---
+++ src/main/java/com/jo/algorithm/leetcode/hot100/LeetCod35.java
@@ -0,0 +1,26 @@
+package com.jo.algorithm.leetcode.hot100;
+
+/**
+ * Date:2024/11/10
+ * Author:Jo
+ * Description:35. 搜索插入位置
+ */
+public class LeetCod35 {
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int middle = left + (right - left) / 2;
+                if (target > nums[middle]) {
+                    left = middle + 1;
+                } else if (target < nums[middle]) {
+                    right = middle - 1;
+                } else {
+                    return middle;
+                }
+            }
+            return left;
+        }
+    }
+}
