78e686ad2ea88a152606a2dc9e7aec64dfaf74a3
Joyin801 <yxq801@gmail.com>
2025-01-29 22:51:53 +0800

    LeetCode1 update

--- src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode1.java
+++ src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode1.java
@@ -1,9 +1,26 @@
 package com.jo.algorithm.leetcode.hot100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Date:2024/11/2
  * Author:Jo
  * Description:1.
  */
 public class LeetCode1 {
+    //    示例 1：
+//    输入：nums = [2,7,11,15], target = 9
+//    输出：[0,1]
+//    解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
 }
