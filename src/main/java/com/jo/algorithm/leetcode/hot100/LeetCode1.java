eec3d74ca71f944fa08ceed5e4a91ad024de6f1e
Joyin801 <yxq801@gmail.com>
2025-02-11 00:58:40 +0800

    LeetCode1 update

--- src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode1.java
+++ src/main/java/com/jo/algorithm/leetcode/hot100/LeetCode1.java
@@ -9,10 +9,16 @@ import java.util.Map;
  * Description:1.
  */
 public class LeetCode1 {
-    //    示例 1：
-//    输入：nums = [2,7,11,15], target = 9
-//    输出：[0,1]
-//    解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+//    Given an array of integers nums and an integer target,
+//    return indices of the two numbers such that they add up to target.
+//    You may assume that each input would have exactly one solution,
+//    and you may not use the same element twice.
+//    You can return the answer in any order.
+
+//    Example 1:
+//    Input: nums = [2,7,11,15], target = 9
+//    Output: [0,1]
+//    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {
