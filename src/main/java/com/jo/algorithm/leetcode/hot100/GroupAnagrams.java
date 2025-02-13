47f9fd22017452a0d4825813c22ceb1cfd3c9879
Joyin801 <yxq801@gmail.com>
2025-02-13 23:49:25 +0800

    49. Group Anagrams

---
+++ src/main/java/com/jo/algorithm/leetcode/hot100/GroupAnagrams.java
@@ -0,0 +1,25 @@
+package com.jo.algorithm.leetcode.hot100;
+
+import java.util.*;
+
+/**
+ * Date:2025/2/13
+ * Author:Jo
+ * Description:49. Group Anagrams
+ */
+public class GroupAnagrams {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            HashMap<String, List<String>> map = new HashMap<>();
+            for (String str : strs) {
+                char[] chars = str.toCharArray();
+                Arrays.sort(chars);
+                String key = new String(chars);
+                List<String> list = map.getOrDefault(key, new ArrayList<>());
+                list.add(str);
+                map.put(key, list);
+            }
+            return new ArrayList<>(map.values());
+        }
+    }
+}
