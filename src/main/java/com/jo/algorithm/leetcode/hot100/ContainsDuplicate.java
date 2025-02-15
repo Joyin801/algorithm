af49c3d4f19b7ed2fc61e21dd17b49b5731935ad
Joyin801 <yxq801@gmail.com>
2025-02-15 23:59:44 +0800

    217. Contains Duplicate

---
+++ src/main/java/com/jo/algorithm/leetcode/hot100/ContainsDuplicate.java
@@ -0,0 +1,47 @@
+package com.jo.algorithm.leetcode.hot100;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Date:2025/2/15
+ * Author:Jo
+ * Description:217. Contains Duplicate
+ */
+public class ContainsDuplicate {
+    class Solution1 {
+        public boolean containsDuplicate(int[] nums) {
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i + 1]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    class Solution2 {
+        public boolean containsDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            return nums.length != set.size();
+        }
+    }
+
+    class Solution3 {
+        //用时间最少的方法
+        public boolean containsDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                if (!set.add(num)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
