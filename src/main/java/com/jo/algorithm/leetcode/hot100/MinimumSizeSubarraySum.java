af17ead3fdb7917dd028c7e24554d400e78d79f0
Joyin801 <yxq801@gmail.com>
2025-02-17 23:56:06 +0800

    209. Minimum Size Subarray Sum

--- src/main/java/com/jo/algorithm/leetcode/hot100/MinimumSizeSubarraySum.java
+++ src/main/java/com/jo/algorithm/leetcode/hot100/MinimumSizeSubarraySum.java
@@ -7,8 +7,21 @@ package com.jo.algorithm.leetcode.hot100;
  */
 public class MinimumSizeSubarraySum {
     class Solution {
+        //时间复杂度O(n)
+        //空间复杂度O(1)
         public int minSubArrayLen(int target, int[] nums) {
-
+            int ans = nums.length + 1;
+            int left = 0;
+            int sum = 0;
+            for (int right = 0; right < nums.length; right++) {
+                sum += nums[right];
+                while (sum >= target) {
+                    ans = Math.min(ans, right - left + 1);
+                    sum -= nums[left];
+                    ++left;
+                }
+            }
+            return ans >= nums.length + 1 ? 0 : ans;
         }
     }
 }
