05dc511e6b87ac3a7b5eb7a1da8094b218a21772
Joyin801 <yxq801@gmail.com>
2025-02-22 20:49:38 +0800

    15. 3Sum

--- src/main/java/com/jo/algorithm/leetcode/hot100/ThreeSum.java
+++ src/main/java/com/jo/algorithm/leetcode/hot100/ThreeSum.java
@@ -14,6 +14,49 @@ public class ThreeSum {
     //    such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
     //    Notice that the solution set must not contain duplicate triplets.
     class Solution {
+        public List<List<Integer>> threeSumV2(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            Arrays.sort(nums);
+            int len = nums.length;
+            for (int i = 0; i < len - 2; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[i + 1] + nums[i + 2] > 0) {
+                    break;
+                }
+                if (nums[i] + nums[len - 2] + nums[len - 1] < 0) {
+                    continue;
+                }
+                int j = i + 1;
+                int k = len - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum < 0) {
+                        j++;
+                    } else if (sum > 0) {
+                        k--;
+                    } else {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        res.add(list);
+                        j++;
+                        while (j < k && nums[j] == nums[j - 1]) {
+                            j++;
+                        }
+                        k--;
+                        while (j < k && nums[k] == nums[k + 1]) {
+                            k--;
+                        }
+                    }
+                }
+
+            }
+            return res;
+        }
+
         public List<List<Integer>> threeSum(int[] nums) {
             List<List<Integer>> result = new ArrayList<>();
             Arrays.sort(nums);
