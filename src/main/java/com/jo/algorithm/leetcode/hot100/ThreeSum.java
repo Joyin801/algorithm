5a16ead2052f2ec5a77e35e5b643ebfc0b328ef5
Joyin801 <yxq801@gmail.com>
2025-03-09 23:18:53 +0800

    update

--- src/main/java/com/jo/algorithm/leetcode/hot100/ThreeSum.java
+++ src/main/java/com/jo/algorithm/leetcode/hot100/ThreeSum.java
@@ -102,5 +102,47 @@ public class ThreeSum {
             }
             return result;
         }
+
+        public List<List<Integer>> threeSumIII(int[] nums) {
+            List<List<Integer>> ans = new ArrayList();
+            Arrays.sort(nums);
+            int len = nums.length;
+            for (int i = 0; i < len - 2; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[i + 1] + nums[i + 2] > 0) {
+                    break;
+                }
+                if (nums[i] + nums[len - 1] + nums[len - 2] < 0) {
+                    continue;
+                }
+                int j = i + 1;
+                int k = len - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum > 0) {
+                        k--;
+                    } else if (sum < 0) {
+                        j++;
+                    } else {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        ans.add(list);
+                        j++;
+                        while (j < k && nums[j] == nums[j - 1]) {
+                            j++;
+                        }
+                        k--;
+                        while (j < k && nums[k] == nums[k + 1]) {
+                            k--;
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
